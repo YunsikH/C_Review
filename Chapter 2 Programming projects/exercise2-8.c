a21568784a0ffcffaccb23385abfa3aa8706e159
Yunsik Han <yunsikhan.yh@gmail.com>
2024-06-28 11:49:46 +1200

    finished exercise2-8.c

--- Chapter 2 Programming projects/exercise2-8.c
+++ Chapter 2 Programming projects/exercise2-8.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    float loan_amount, int_rate, monthly_payment;
+    float loan_amount, int_rate, monthly_payment, monthly_rate;
     
     printf("Enter amount of loan: ");
     scanf("%f", &loan_amount);
@@ -10,6 +10,13 @@ int main(void)
     scanf("%f", &int_rate);
     printf("Enter monthly payment: ");
     scanf("%f", &monthly_payment);
+
+    loan_amount += (loan_amount * (int_rate / 100) / 12);
+    printf("Balance remaining after first payment: $%.2f\n", loan_amount -= monthly_payment);
+    loan_amount += (loan_amount * (int_rate / 100) / 12);
+    printf("Balance remaining after second payment: $%.2f\n",  loan_amount -= monthly_payment);
+    loan_amount += (loan_amount * (int_rate / 100) / 12);
+    printf("Balance remaining after third payment: $%.2f\n",  loan_amount -= monthly_payment);
 
     return 0;
 }
