cccf59ba86960df91f3342b23384ea77c9103b52
Yunsik Han <yunsikhan.yh@gmail.com>
2024-07-01 11:21:03 +1200

    finished exercise3-5

--- Chapter 3/exercise3-5.c
+++ Chapter 3/exercise3-5.c
@@ -7,13 +7,13 @@ int main(void)
     int p9, p10, p11, p12;
     int p13, p14, p15, p16;
 
-    printf("Enter Numbers from 1 to 16 in any order");
+    printf("Enter Numbers from 1 to 16 in any order: ");
     scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &p1, &p2, &p3, &p4, &p5, &p6, &p7, &p8, &p9, &p10, &p11, &p12, &p13, &p14, &p15, &p16);
 
-    printf("%d %d %d %d\n%d %d %d %d\n%d %d %d %d\n%d %d %d %d\n", p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16);
-    printf("Row Sums: %d %d %d %d", p1 + p2 + p3 + p4);
-    printf("Column Sums: ");       
-    printf("Diagonal Sums: ");
+    printf("%2d %2d %2d %2d\n%2d %2d %2d %2d\n%2d %2d %2d %2d\n%2d %2d %2d %2d\n", p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16);
+    printf("Row Sums: %d %d %d %d\n", p1 + p2 + p3 + p4, p5 + p6 + p7 + p8, p9 + p10 + p11 + p12, p13 + p14 + p15 + p16);
+    printf("Column Sums: %d %d %d %d\n", p1 + p5 + p9 + p13, p2 + p6 + p10 + p14, p3 + p7 + p11 + p15, p4 + p8 + p12 + p16);       
+    printf("Diagonal Sums: %d %d\n", p1 + p6 + p11 + p16, p4 + p7 + p10 + p13);
 
     return 0;
 }
