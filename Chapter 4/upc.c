d10d1a9ca463a360db61d457ac2e2ce7f0ee425b
Yunsik Han <yunsikhan.yh@gmail.com>
2024-07-02 10:06:09 +1200

    finished missing code

--- Chapter 4/upc.c
+++ Chapter 4/upc.c
@@ -11,6 +11,14 @@ int main(void)
     scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
     printf("Enter second group of five digits: ");
     scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
+    
+    first_sum = d + i2 + i4 + j1 + j3 + j5;
+    second_sum = i1 + i3 + i5 + j2 + j4;
+    total = 3 * first_sum + second_sum;
+
+    printf("Check digit: %d\n", 9 - ((total - 1) % 10));
+
+    return 0;
 }
 
 //conditional expression:
