eb50b7a6aee2d8a8d886af023530822051122448
Yunsik Han <yunsikhan.yh@gmail.com>
2024-07-22 11:52:19 +1200

    error where random input comes outta nowhere

--- Chapter 8/exercise8-14.c
+++ Chapter 8/exercise8-14.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 #define SENTENCE_LENGTH 100
 
@@ -7,27 +8,30 @@ int main (void)
     char ch, terminating, sentence[SENTENCE_LENGTH];
     int i, j, prev;
 
-    for ( i= 0; i < (int) sizeof(sentence)/sizeof(sentence[0]) && sentence[i - 1] != '\n'; i++)
+    for ( i = 0; i < (int) sizeof(sentence)/sizeof(sentence[0]) && sentence[i - 1] != '\n'; i++)
     {
         ch = getchar();
                 
         if (ch == '.' || ch == '?' || ch == '!')
         {
             terminating = ch;
+            prev = i;
         }
         else
             sentence[i] = ch;
     }
     
-    //prev = (int) sizeof(sentence)/sizeof(sentence[0]);
     
-    for (i = (int) sizeof(sentence)/sizeof(sentence[0]) - 1; i >= 0; i--)
+    
+    for ( i = 0; i < (int) sizeof(sentence)/sizeof(sentence[0]) && sentence[i - 1] != '\n'; i++)
     {
-        
-        if (sentence[i] == ' ')
+        if (tolower(sentence[i])  'a' || tolower(sentence[i]) == '?' || tolower(sentence[i])i] == '!')
         {
-            printf("lomao");
+            terminating = ch;
+            prev = i;
         }
+        else
+            sentence[i] = ch;
     }
 
     return 0;
