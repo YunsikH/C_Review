905e5f5bd0b1a75d74ed20ba80b6ae66d79e563f
Yunsik Han <yunsikhan.yh@gmail.com>
2024-07-24 11:15:59 +1200

    finished exercise

--- Chapter 8/exercise8-15.c
+++ Chapter 8/exercise8-15.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
+#include <ctype.h>
 
 #define SENTENCE_LENGTH 80
+//#define TERM_CHAR_NEWLINE_SIZE 2 //The terminating character and the newline at the end of sentence char array
 
-int main(void)
+int main (void)
 {
     char ch, terminating, sentence[SENTENCE_LENGTH];
-    int i, j, prev;
+    int i, j, no_of_characters, shift;
+
+    printf("Enter message to be encrypted: ");
+    for ( i = 0; i < (int) sizeof(sentence)/sizeof(sentence[0]); i++)
+    {
+        sentence[i] = getchar();
+        if (sentence[i] == '\n')
+        {
+            break;
+        }
+    }
+    
+    no_of_characters = i;
+    
+    printf("Enter shift amount (1-25): ");
+    scanf("%d", &shift);
 
     for ( i = 0; i < (int) sizeof(sentence)/sizeof(sentence[0]) && sentence[i - 1] != '\n'; i++)
+    {
+        if (sentence[i] >= 'a' && sentence[i] <= 'z') 
         {
-            ch = getchar();
-                    
-            if (ch == '.' || ch == '?' || ch == '!')
-            {
-                terminating = ch;
-                prev = i;
-            }
-            else
-                sentence[i] = ch;
+            sentence[i] = ((sentence[i] - 'a') + shift) % 26 + 'a';
         }
+        else if (sentence[i] >= 'A' && sentence[i] <= 'Z')
+        {
+            sentence[i] = ((sentence[i] - 'A') + shift) % 26 + 'A';
+        }
+    }
+
+    printf("Encrypted message: ");
+    
+    for ( i = 0; i < no_of_characters; i++)
+    {
+            printf("%c", sentence[i]);
+    }
 
     return 0;
 }
