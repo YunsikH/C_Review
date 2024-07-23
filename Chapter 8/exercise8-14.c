cc156c42f5e8e5a4911f748065507c977908d187
Yunsik Han <yunsikhan.yh@gmail.com>
2024-07-24 10:52:04 +1200

    finished exercise

--- Chapter 8/exercise8-14.c
+++ Chapter 8/exercise8-14.c
@@ -2,11 +2,14 @@
 #include <ctype.h>
 
 #define SENTENCE_LENGTH 100
+#define TERM_CHAR_NEWLINE_SIZE 2 //The terminating character and the newline at the end of sentence char array
 
 int main (void)
 {
     char ch, terminating, sentence[SENTENCE_LENGTH];
-    int i, j, no_of_characters, count;
+    int i, j, no_of_characters;
+
+    printf("Enter a sentence: ");
 
     for ( i = 0; i < (int) sizeof(sentence)/sizeof(sentence[0]) && sentence[i - 1] != '\n'; i++)
     {
@@ -16,16 +19,15 @@ int main (void)
         {
             terminating = sentence[i];
         }
-        
-        no_of_characters = i;
     }
     
-    no_of_characters-1;
-    for ( i = no_of_characters - 1; i >= -1; i--)
+    no_of_characters = i - TERM_CHAR_NEWLINE_SIZE;
+    
+    printf("Reversal of sentence: ");
+    for ( i = no_of_characters; i >= -1; i--)
     {
         if (sentence[i] == ' ' || i == -1)
         {
-
             for (j = i + 1; j < no_of_characters; j++)
             {
                 printf("%c", sentence[j]);
@@ -39,7 +41,6 @@ int main (void)
             no_of_characters = i;
         }
     }
-    
 
     return 0;
 }
