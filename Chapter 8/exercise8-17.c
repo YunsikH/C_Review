b26d5be4393f2d85dbbefa0c6ce2452c767693c4
Yunsik Han <yunsikhan.yh@gmail.com>
2024-07-25 10:37:31 +1200

    Prints all numbers in a square without 0s, working on dropping down if next position is filled

--- Chapter 8/exercise8-17.c
+++ Chapter 8/exercise8-17.c
@@ -2,34 +2,54 @@
 
 int main(void)
 {
-    int n, magic_square[n][n], row_pos, col_pos, i;
+    int n, row_pos, col_pos, i, j;
 
-    printf("Enter size of magic square");
+    printf("Enter size of magic square: ");
     scanf("%d", &n);
 
+    int magic_square[n][n];
     row_pos = n / 2;
     col_pos = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            magic_square[j][i] = 0;
+        }
+    }
+
     for (i = 1; i <= (n * n); i++)
     {
         magic_square[row_pos][col_pos] = i;
 
         row_pos++;
         col_pos--;
+        
+        //checking to see if magic square is out of bounds 
         if (row_pos == n)
             row_pos = 0;
 
         if (col_pos < 0)
             col_pos = n - 1;
         
-        if (magic_square[row_pos][])
+        if (magic_square[row_pos][col_pos] > 0)
         {
-            /* code */
+            col_pos++;
         }
-        
-
-
     }
 
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%4d", magic_square[j][i]);
+        }
+
+        printf("\n");
+    }
+    /*
+    */
 
     return 0;
 }
