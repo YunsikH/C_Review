569808eb0e7ca7c63c5a7600fff5991c067d2fd6
Yunsik Han <yunsikhan.yh@gmail.com>
2024-07-29 11:48:06 +1200

    Started craps game with pseudocode, finished roll_dice function, working on play_game function, and main

---
+++ Chapter 9/exercise9-8.c
@@ -0,0 +1,83 @@
+/*****
+ * Craps Game
+ * 
+ * Played with two dice
+ * On first roll, winning sum of both dice of 7 or 11
+ * On first roll, losing roll sum is 2, 3 , 12
+ * If first roll is not winning or losing, first roll is now point roll
+ * 
+ * After first roll, next winning roll is point roll
+ * After first roll, next losing roll is 7
+ * after roll, any not winning or not losing dice means next roll
+ * 
+ * after the end of a game(win or lose condition is met), 
+ * program will ask if user wants to play again.
+ * to play again, user must respond with y or Y
+ * any other input will display the number of iwns and then terminate
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define DIE_SIZE 6
+
+int roll_dice(void);
+bool play_game(void);
+
+
+/**
+ * Main calls play_game until the user wants to stop
+ * Main will keep track of the number of wins and losses
+ * and will also display the you win or you lose messages
+ */
+int main(void)
+{
+    srand((unsigned) time(NULL));
+    bool outcome, running = true;
+    int wins = 0, losses = 0;
+    while(running)
+    {
+        outcome = play_game();
+        if (outcome == false)
+            losses++;
+        else if (outcome == true)
+            wins++;
+    }
+
+    return 0;
+}
+/***
+ * roll_dice will simply roll the dice and be called in play_game function
+ */
+int roll_dice(void)
+{
+    return (rand() % DIE_SIZE) + 1 + (rand() % DIE_SIZE + 1);
+}
+
+/***
+ * play_game will play one game and return true if the player wins or false if the player loses
+ * It will display the messages showing the results of the players rolls
+ */
+bool play_game(void)
+{
+    //bool outcome;
+    int point_roll, first_roll;
+
+    first_roll = roll_dice();
+    printf("You rolled: %d\n", first_roll);
+    if (first_roll == 7 || first_roll == 11)//winning on first roll
+    {
+        printf("You Win!\n\n");
+        return true;
+    }
+    else if (first_roll == 2 || first_roll == 3 || first_roll == 12) //losing on first roll
+    {
+
+        return false;
+    }
+    printf("Your point is %d\n", point_roll);
+
+
+}
