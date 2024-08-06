44b792d89ed4bdfb39006800efdcc035479c1eb2
Yunsik Han <yunsikhan.yh@gmail.com>
2024-08-07 10:46:18 +1200

    added code block in main to take users input and accept only numbers, working on process_digit function

--- Chapter 10/exercise10-7.c
+++ Chapter 10/exercise10-7.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #define MAX_DIGITS 10
+#define SEGMENT_HEIGHT 4
 
 /*
       _  <-0
@@ -24,23 +25,57 @@ int segments[MAX_DIGITS][7] =
     //0th, 3rd and 6th element are _, the rest are |
 };
 
-//this will save the displayed 
-char digits[4][MAX_DIGITS * 4];
+//this will save the digit segment display 
+char digits[SEGMENT_HEIGHT][MAX_DIGITS * SEGMENT_HEIGHT];
 
-//Goes through the digits array and initialises all elements to space
 void clear_digits_array(void);
-
-/*
-    reads the digit input, converts it using segment, and saves the segmented digit into digits
-*/
 void process_digit(int digit, int position);
-
-/*
-    prints the digits array
-*/
 void print_digits_array(void);
 
 int main(void)
+{
+    char ch;
+    int count = 0, digit;
+
+    clear_digits_array();
+
+    printf("Enter a number: ");
+    
+    while ((ch = getchar()) != '\n')
+    {
+        if(ch >= '0' && ch <= '9' && count < MAX_DIGITS)
+        {
+            process_digit(ch - '0', count);
+            count++;
+        }
+    }
+
+    print_digits_array();
+
+    return 0;
+}
+
+//Goes through the digits array and initialises all elements to space
+void clear_digits_array(void)
+{
+    int x, y;
+    for (x = 0; x < SEGMENT_HEIGHT; x++)
+    {
+        for (y = 0; y < MAX_DIGITS * SEGMENT_HEIGHT; y++)
+        {
+            digits[x][y] = ' ';
+        }
+    }
+}
+/*
+    reads the digit input, converts it using segment, and saves the segmented digit into digits
+*/
+void process_digit(int digit, int position)
+{
+
+}
+
+void print_digits_array(void)
 {
 
 }
