a2480805f6588a336a777412b02c827c684f3c6d
Yunsik Han <yunsikhan.yh@gmail.com>
2024-08-09 11:25:29 +1200

    finished functionality of fraction lcd creator

--- Chapter 11/exercise11-3.c
+++ Chapter 11/exercise11-3.c
@@ -9,34 +9,26 @@ int main(void)
 
     printf("Enter a fraction: ");
     scanf("%d/%d", &n1, &n2);
-    m = n1;
-    n = n2;
-    //cheat way
-    /*
-    for (int i = 1; i <= i1 && i <= i2; ++i)
-    {
-        if (i1 % i == 0 && i2 % i == 0)
-            gcd = i;
-    }
-    */
+
     reduce(n1, n2, &m, &n);
 
-    printf("In lowest terms: %d/%d", n1 / m, n2 / m);
+    printf("In lowest terms: %d/%d", m, n);
 
     return 0;
 }
 
 void reduce(int numerator, int denominator, int *reduced_numerator, int *reduced_denominator)
 {
-    int remainder, gcd;
+    int remainder, gcd = numerator, n = denominator;
+    *reduced_numerator = numerator, *reduced_denominator = denominator;
     
-    if (denominator == 0)
-        gcd = numerator;
-    else
-    while (denominator != 0)
+    while (n != 0)
     {
-            remainder = numerator % denominator;
-            numerator = denominator;
-            denominator = remainder;
-    }
+        remainder = gcd % n;
+        gcd = n;
+        n = remainder;
+    } 
+
+    *reduced_numerator /= gcd;
+    *reduced_denominator /=  gcd;
 }
